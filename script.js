6c1af6a045669438e80681bddd551fbb984bdd77
notBhavik <154809700+notBhavik@users.noreply.github.com>
2024-04-02 17:45:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,69 +1,27 @@
-console.log('Welcome to 🌡️ Temperature Converter');
+const celciusEl = document.getElementById("Celcius");
+const fahrenheitEl = document.getElementById("Fahrenheit");
+const kelvinEl = document.getElementById("Kelvin");
 
-const tempLoad = () => {
-    let fa = document.getElementById('fa');
-    fa.innerHTML = "&#xf2cb";
-    fa.style.color = "#ffa41b";
+function computeTemp(event) {
+    const currentValue = event.target.value;
 
-    setTimeout(() => {
-        fa.innerHTML = "&#xf2ca;";
-        fa.style.color = "#ffa41b";
-    }, 1000)
+    switch (event.target.name) {
+        case "Celcius":
+            fahrenheitEl.value = (currentValue * 9 / 5) + 32;
+            kelvinEl.value = parseFloat(currentValue) + 273.15;
+            break;
 
-    setTimeout(() => {
-        fa.innerHTML = "&#xf2c9;";
-    }, 2000)
+        case "Fahrenheit":
+            celciusEl.value = (currentValue - 32) * 5 / 9;
+            kelvinEl.value = (currentValue - 32) * 5 / 9 + 273.15;
+            break;
 
-    setTimeout(() => {
-        fa.innerHTML = "&#xf2c8;";
-    }, 3000)
+        case "Kelvin":
+            celciusEl.value = currentValue - 273.15;
+            fahrenheitEl.value = (currentValue - 273.15) * 9 / 5 + 32;
+            break;
 
-    setTimeout(() => {
-        fa.innerHTML = "&#xf2c7;";
-        fa.style.color = "#ff5151";
-    }, 4000)
+        default:
+            break;
+    }
 }
-
-setInterval(() => {
-    fa.style.color = "#ffa41b";
-    tempLoad();
-}, 5000);
-
-
-tempLoad();
-
-const calculateTemp = () => {
-    const numberTemp = document.getElementById('temp').value;
-    // console.log(numberTemp);
-
-    const tempSelected = document.querySelector('#temp_diff');
-    const valeTemp = temp_diff.options[tempSelected.selectedIndex].value;
-    // console.log(valeTemp);
-
-
-    // Convert temperature from Celcius to Fahrenheit
-    const celTOfah = (cel) => {
-        let fahrenheit = (cel * (9 / 5) + 32);
-        return fahrenheit;
-    }
-
-    // Convert temperature from Fahrenheit to Celsius
-    const fahTOcel = (fehr) => {
-        let celsius = ((fehr - 32) * 5 / 9);
-        return celsius;
-    }
-
-    let result;
-    if (valeTemp == "cel") {
-        result = celTOfah(numberTemp);
-        document.getElementById('resultContainer').innerHTML = `= ${result}°Fahrenheit`;
-    } else {
-        result = fahTOcel(numberTemp);
-        document.getElementById('resultContainer').innerHTML = `= ${result}°Celsius`;
-    }
-
-    setTimeout(() => {
-        window.location.reload();
-    }, 1500);
-}
-
